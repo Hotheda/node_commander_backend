97d3b09f2ef285451882b0f7d43e42fad6491d42
Heda <hotheda@gmail.com>
2020-09-19 01:49:07 +0200

    small fixes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const config = require('./config.js')
 
-const bodyParser = require('body-parser')
+//const bodyParser = require('body-parser')
 
 const app = express();
 app.use(express.json());
